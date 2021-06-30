e3650051f32e2d3c8e80594607ecb42fa50de187
Jen1618 <jcano2@oxy.edu>
2021-06-30 09:46:56 -0700

    new changes to frontend - not done yet

---
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+  blogs: any[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
